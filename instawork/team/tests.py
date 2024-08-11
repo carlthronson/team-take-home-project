34efe07fc7f1f7b47d7de164d2ebc5d2b7ef4ca2
Carl Thronson <carlthronson@gmail.com>
2024-08-10 18:06:08 -0700

    Add a test case to ensure default role is Regular

--- instawork/team/tests.py
+++ instawork/team/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import TeamMember
 
 # Create your tests here.
+class TeamMemberTestCase(TestCase):
+    def setUp(self):
+        self.team_member = TeamMember.objects.create(
+          first_name="Carl",
+          last_name="Thronson",
+          email="carlthronson@gmail.com",
+          phone_number="+14082346237")
+
+    def test_default_role_is_regular(self):
+        """TeamMembers created without role are correctly defaulted to regular"""
+        carl = self.team_member
+        self.assertEqual(carl.role, False)
